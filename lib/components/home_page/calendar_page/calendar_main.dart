0704551d275191c1fb1c4009e88795fc6c9c278e
maciejdrobnik <maciejdrobnik34@gmail.com>
2023-03-09 15:52:16 +0100

    Optimazation of Calendar widget

--- lib/components/home_page/calendar_page/calendar_main.dart
+++ lib/components/home_page/calendar_page/calendar_main.dart
@@ -2,7 +2,6 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:sport_manager/services/convert_timestamp.dart';
-import 'package:sport_manager/services/firebase_service.dart';
 import 'package:table_calendar/table_calendar.dart';
 import '../../../assets/constants.dart' as constants;
 import '../../../classes/task_model.dart';
@@ -10,7 +9,11 @@ import '../home_blue_button.dart';
 import 'calendar_task.dart';
 
 class CalendarMain extends StatefulWidget {
-  const CalendarMain({Key? key}) : super(key: key);
+  final BoxConstraints parentConstraints;
+  final LinkedHashMap<DateTime, List<TaskModel>> events;
+  const CalendarMain(
+      {Key? key, required this.parentConstraints, required this.events})
+      : super(key: key);
 
   @override
   State<CalendarMain> createState() => _CalendarMainState();
@@ -18,10 +21,8 @@ class CalendarMain extends StatefulWidget {
 
 class _CalendarMainState extends State<CalendarMain> {
   ConvertTimestampService timeService = ConvertTimestampService();
-  FirebaseService service = FirebaseService();
   late DateTime selectedDay;
   String month = "";
-  late LinkedHashMap<DateTime, List<TaskModel>> events;
   var kEvents = <DateTime, List<TaskModel>>{};
 
   @override
@@ -52,106 +53,87 @@ class _CalendarMainState extends State<CalendarMain> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return FutureBuilder(
-          future: service.getAllUserEvents(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                events = snapshot.data!;
-                kEvents = LinkedHashMap<DateTime, List<TaskModel>>(
-                  equals: isSameDay,
-                  hashCode: getHashCode,
-                )..addAll(events);
-                return Column(
-                  children: [
-                    Flexible(
-                      flex: constants.calendarBlueButtonFlex,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            top: constants.calendarPaddingTop *
-                                constraints.maxHeight),
-                        child: const HomeBlueButton(
-                            content: "Calendar",
-                            minBlueButtonFontSize:
-                                constants.minBlueButtonFontSize,
-                            maxBlueButtonFontSize:
-                                constants.maxBlueButtonFontSize),
-                      ),
+    kEvents = LinkedHashMap<DateTime, List<TaskModel>>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(widget.events);
+    return Column(
+      children: [
+        Flexible(
+          flex: constants.calendarBlueButtonFlex,
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: constants.calendarPaddingTop *
+                    widget.parentConstraints.maxHeight),
+            child: const HomeBlueButton(
+                content: "Calendar",
+                minBlueButtonFontSize: constants.minBlueButtonFontSize,
+                maxBlueButtonFontSize: constants.maxBlueButtonFontSize),
+          ),
+        ),
+        Flexible(
+          flex: constants.calendarFlex,
+          child: Column(
+            children: [
+              TableCalendar<TaskModel>(
+                focusedDay: selectedDay,
+                eventLoader: _getEventsForDay,
+                firstDay: DateTime(2022, 2, 27),
+                lastDay: DateTime(2024, 3, 5),
+                calendarFormat: CalendarFormat.week,
+                rowHeight: constants.calendarRowHeight,
+                onDaySelected: _changeSelectedDay,
+                selectedDayPredicate: (day) => isSameDay(day, selectedDay),
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                //! The styling of calendar. We will need to discuss it. It has some strange behaviours
+                calendarStyle: const CalendarStyle(
+                    isTodayHighlighted: false,
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.yellow,
+                      shape: BoxShape.circle,
                     ),
-                    Flexible(
-                      flex: constants.calendarFlex,
-                      child: Column(
-                        children: [
-                          TableCalendar<TaskModel>(
-                            focusedDay: selectedDay,
-                            eventLoader: _getEventsForDay,
-                            firstDay: DateTime(2022, 2, 27),
-                            lastDay: DateTime(2024, 3, 5),
-                            calendarFormat: CalendarFormat.week,
-                            rowHeight: constants.calendarRowHeight,
-                            onDaySelected: _changeSelectedDay,
-                            selectedDayPredicate: (day) =>
-                                isSameDay(day, selectedDay),
-                            startingDayOfWeek: StartingDayOfWeek.monday,
-                            //! The styling of calendar. We will need to discuss it. It has some strange behaviours
-                            calendarStyle: const CalendarStyle(
-                                isTodayHighlighted: false,
-                                selectedDecoration: BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.circle,
-                                ),
-                                cellMargin: EdgeInsets.all(4),
-                                defaultTextStyle: TextStyle(fontSize: 12),
-                                todayTextStyle: TextStyle(fontSize: 12),
-                                outsideTextStyle: TextStyle(fontSize: 12),
-                                selectedTextStyle: TextStyle(fontSize: 12),
-                                weekendTextStyle: TextStyle(fontSize: 12)),
-                            headerStyle: HeaderStyle(
-                              titleCentered: true,
-                              formatButtonVisible: false,
-                              headerPadding: EdgeInsets.only(
-                                  left: constants.calendarMonthTextPaddingLeft *
-                                      constraints.maxWidth,
-                                  top: constants.calendarMonthTextPaddingTop *
-                                      constraints.maxHeight,
-                                  bottom: 6,
-                                  right: 10),
-                              // leftChevronVisible: false,
-                              // rightChevronVisible: false,
-                              leftChevronPadding: const EdgeInsets.all(0),
-                              leftChevronMargin: const EdgeInsets.all(0),
-                              leftChevronIcon: const Icon(
-                                Icons.chevron_left,
-                                size: 19,
-                              ),
-                              rightChevronPadding: const EdgeInsets.all(0),
-                              rightChevronMargin: const EdgeInsets.all(0),
-                              rightChevronIcon: const Icon(
-                                Icons.chevron_right,
-                                size: 19,
-                              ),
-                              titleTextStyle: const TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                        flex: constants.calendarTaskFlex,
-                        child: CalendarTask(
-                          parentConstraints: constraints,
-                          selectedDate: selectedDay,
-                        )),
-                  ],
-                );
-              } else {
-                return const Text("Something went wrong");
-              }
-            } else {
-              return const Text("Wait a Second");
-            }
-          });
-    });
+                    cellMargin: EdgeInsets.all(4),
+                    defaultTextStyle: TextStyle(fontSize: 12),
+                    todayTextStyle: TextStyle(fontSize: 12),
+                    outsideTextStyle: TextStyle(fontSize: 12),
+                    selectedTextStyle: TextStyle(fontSize: 12),
+                    weekendTextStyle: TextStyle(fontSize: 12)),
+                headerStyle: HeaderStyle(
+                  titleCentered: true,
+                  formatButtonVisible: false,
+                  headerPadding: EdgeInsets.only(
+                      left: constants.calendarMonthTextPaddingLeft *
+                          widget.parentConstraints.maxWidth,
+                      top: constants.calendarMonthTextPaddingTop *
+                          widget.parentConstraints.maxHeight,
+                      bottom: 6,
+                      right: 10),
+                  leftChevronPadding: const EdgeInsets.all(0),
+                  leftChevronMargin: const EdgeInsets.all(0),
+                  leftChevronIcon: const Icon(
+                    Icons.chevron_left,
+                    size: 19,
+                  ),
+                  rightChevronPadding: const EdgeInsets.all(0),
+                  rightChevronMargin: const EdgeInsets.all(0),
+                  rightChevronIcon: const Icon(
+                    Icons.chevron_right,
+                    size: 19,
+                  ),
+                  titleTextStyle: const TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+            flex: constants.calendarTaskFlex,
+            child: CalendarTask(
+              parentConstraints: widget.parentConstraints,
+              selectedDate: selectedDay,
+              tasks: _getEventsForDay(selectedDay),
+            )),
+      ],
+    );
   }
 }
--- lib/components/home_page/calendar_page/calendar_task.dart
+++ lib/components/home_page/calendar_page/calendar_task.dart
@@ -1,118 +1,98 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:sport_manager/classes/task_model.dart';
 import 'package:sport_manager/services/convert_timestamp.dart';
-import 'package:sport_manager/services/firebase_service.dart';
 import '../../../assets/constants.dart' as constants;
 
 class CalendarTask extends StatelessWidget {
   final BoxConstraints parentConstraints;
   final DateTime selectedDate;
+  final List<TaskModel> tasks;
   const CalendarTask(
-      {super.key, required this.parentConstraints, required this.selectedDate});
+      {super.key,
+      required this.parentConstraints,
+      required this.selectedDate,
+      required this.tasks});
 
   @override
   Widget build(BuildContext context) {
-    FirebaseService service = FirebaseService();
     ConvertTimestampService timeService = ConvertTimestampService();
-    return FutureBuilder(
-      future: service.getTasks(selectedDate),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasData) {
-            if (snapshot.data!.isNotEmpty) {
-              return Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Flexible(
-                      flex: constants.calendarTaskWhiteBoxFlex,
-                      child: Container()),
-                  Flexible(
-                    flex: constants.calendarTaskYellowBoxFlex,
-                    child: Container(
-                      height: constants.calendarTaskYellowBoxHeight *
-                          parentConstraints.maxHeight,
-                      decoration: BoxDecoration(
-                        color: Colors.yellow,
-                        borderRadius: BorderRadius.circular(
-                            constants.calendarYellowBoxBorderRadius),
-                      ),
-                    ),
+    return tasks.isNotEmpty
+        ? Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                  flex: constants.calendarTaskWhiteBoxFlex, child: Container()),
+              Flexible(
+                flex: constants.calendarTaskYellowBoxFlex,
+                child: Container(
+                  height: constants.calendarTaskYellowBoxHeight *
+                      parentConstraints.maxHeight,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(
+                        constants.calendarYellowBoxBorderRadius),
                   ),
-                  Expanded(
-                      flex: constants.calendarTaskNameFlex,
-                      child: Padding(
-                          padding: EdgeInsets.only(
-                              left: parentConstraints.maxWidth *
-                                  constants.calendarTaskNamePaddingLeft),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              AutoSizeText(
-                                snapshot.data![0].name,
-                                minFontSize:
-                                    constants.calendarTaskNameMinFontSize,
-                                maxFontSize:
-                                    constants.calendarTaskNameMaxFontSize,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              AutoSizeText(snapshot.data!.first.description,
-                                  minFontSize: constants
-                                      .calendarTaskDescriptionMinFontSize,
-                                  maxFontSize: constants
-                                      .calendarTaskDescriptionMaxFontSize,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    color:
-                                        const Color(constants.homePageTextColor)
-                                            .withOpacity(
-                                                constants.homePageLowerOpacity),
-                                  ))
-                            ],
-                          ))),
-                  Flexible(
-                      flex: constants.calendarTaskTimeFlex,
+                ),
+              ),
+              Expanded(
+                  flex: constants.calendarTaskNameFlex,
+                  child: Padding(
+                      padding: EdgeInsets.only(
+                          left: parentConstraints.maxWidth *
+                              constants.calendarTaskNamePaddingLeft),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(
-                            padding: EdgeInsets.only(
-                                bottom: constants.calendarBetweenHoursPadding *
-                                    parentConstraints.maxHeight),
-                            child: Text(
-                              timeService.getHourFromTimeStamp(
-                                  snapshot.data!.first.start),
+                          AutoSizeText(
+                            tasks.first.name,
+                            minFontSize: constants.calendarTaskNameMinFontSize,
+                            maxFontSize: constants.calendarTaskNameMaxFontSize,
+                            style: const TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          AutoSizeText(tasks.first.description,
+                              minFontSize:
+                                  constants.calendarTaskDescriptionMinFontSize,
+                              maxFontSize:
+                                  constants.calendarTaskDescriptionMaxFontSize,
                               style: TextStyle(
                                 fontWeight: FontWeight.w400,
                                 color: const Color(constants.homePageTextColor)
                                     .withOpacity(
                                         constants.homePageLowerOpacity),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            timeService
-                                .getHourFromTimeStamp(snapshot.data!.first.end),
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: const Color(constants.homePageTextColor)
-                                  .withOpacity(constants.homePageLowerOpacity),
-                            ),
-                          )
+                              ))
                         ],
-                      ))
-                ],
-              );
-            } else {
-              return const Text("No tasks for today");
-            }
-          } else {
-            return const Text("Something went wrong");
-          }
-        } else {
-          return const Text("Wait a second");
-        }
-      },
-    );
+                      ))),
+              Flexible(
+                  flex: constants.calendarTaskTimeFlex,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            bottom: constants.calendarBetweenHoursPadding *
+                                parentConstraints.maxHeight),
+                        child: Text(
+                          timeService.getHourFromTimeStamp(tasks.first.start),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: const Color(constants.homePageTextColor)
+                                .withOpacity(constants.homePageLowerOpacity),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        timeService.getHourFromTimeStamp(tasks.first.end),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: const Color(constants.homePageTextColor)
+                              .withOpacity(constants.homePageLowerOpacity),
+                        ),
+                      )
+                    ],
+                  ))
+            ],
+          )
+        : const Text("No task for today");
   }
 }
